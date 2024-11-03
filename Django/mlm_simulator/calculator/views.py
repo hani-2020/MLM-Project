9202aa1c4eb0b2d09a8e63f303ae2f2476df2442
hani-2020 <haniroshan2002@gmail.com>
2024-11-04 00:28:10 +0530

    user generation and tree placement

--- Django/mlm_simulator/calculator/views.py
+++ Django/mlm_simulator/calculator/views.py
@@ -1,61 +1,59 @@
 from django.shortcuts import render
 from django.views import View
 from .forms import BinaryForm
-import math
 
 class Member:
 
-    def __init__(self, id, left_value, right_value, parent):
+    def __init__(self, id, parent):
         self.id = id
         self.left_member = None
         self.right_member = None
         self.parent = parent
-        self.left_value = left_value
-        self.right_value = right_value
-    
-    def __str__(self):
-        return f"Member(id={self.id}, range=({self.left_value}, {self.right_value}))"
-
-    def __repr__(self):
-        return self.__str__()
 
 class Tree:
 
-    def __init__(self):
+    def __init__(self, num_members):
         self.root = None
+        self.num_members = num_members
+        #self.members = []
+        self.build_tree()
 
-    def createTree(self, number_of_members):
-        if number_of_members <= 0:
-            return None
-        if self.root is None:
-            right_value = 2 * number_of_members
-            self.root = Member(1, 1, right_value, None) 
-            self._add_members(self.root, number_of_members - 1)
-        return self.root
-
-    def _add_members(self, node, remaining_members):
-        if remaining_members <= 1:
+    def build_tree(self):
+        if self.num_members <= 0:
             return
-        left_id = 2 * node.id
-        right_id = 2 * node.id + 1
+        self.root = Member(id=1, parent=None)
+        #self.members.append(self.root)
+        queue = [self.root]
+        current_id = 2 
+        while current_id <= self.num_members:
+            current_member = queue.pop(0)
+            if current_id <= self.num_members:
+                left_child = Member(id=current_id, parent=current_member)
+                current_member.left_member = left_child
+                queue.append(left_child)
+                #self.members.append(left_child)
+                current_id += 1
+            if current_id <= self.num_members:
+                right_child = Member(id=current_id, parent=current_member)
+                current_member.right_member = right_child
+                queue.append(right_child)
+                #self.members.append(right_child)
+                current_id += 1
 
-        mid_value = (node.left_value + node.right_value) // 2
-        node.left_member = Member(left_id, node.left_value + 1, mid_value, node)
-        node.right_member = Member(right_id, mid_value + 1, node.right_value - 1, node)
-        self._add_members(node.left_member, remaining_members // 2)
-        self._add_members(node.right_member, remaining_members // 2)
+    def display_tree(self):
+        queue = [self.root]
+        while queue:
+            current_member = queue.pop(0)
+            print(f'Member ID: {current_member.id}, ',
+                  f'Left: {current_member.left_member.id if current_member.left_member else None}, ',
+                  f'Right: {current_member.right_member.id if current_member.right_member else None}, ',
+                  f'Parent: {current_member.parent.id if current_member.parent else None}')
+            if current_member.left_member:
+                queue.append(current_member.left_member)
+            if current_member.right_member:
+                queue.append(current_member.right_member)
 
-    def __str__(self):
-        return self._display_tree(self.root)
-
-    def _display_tree(self, node, level=0):
-        if node is None:
-            return ""
-        result = " " * (level * 4) + str(node) + "\n"
-        result += self._display_tree(node.left_member, level + 1)
-        result += self._display_tree(node.right_member, level + 1)
-        return result
-
+            
     
 
 class Calculator(View):
@@ -69,9 +67,8 @@ class Calculator(View):
     
     def post(self, request, *args, **kwargs):
         number_of_users = int(request.POST.get('number_of_users'))
-        tree = Tree()
-        tree.createTree(number_of_users)
-        print(tree)
+        tree = Tree(number_of_users)
+        tree.display_tree()
         pass
         
             
