c22c1a937c8ff1d1a174e1bc366f6ba5b07976df
hani-2020 <haniroshan2002@gmail.com>
2024-11-06 02:50:07 +0530

    lft rgt work started

--- Django/mlm_simulator/calculator/views.py
+++ Django/mlm_simulator/calculator/views.py
@@ -8,45 +8,63 @@ class Member:
         self.id = id
         self.left_member = None
         self.right_member = None
+        self.position = None
         self.parent = parent
+        self.left = None
+        self.right = None
 
 class Tree:
 
     def __init__(self, num_members):
         self.root = None
         self.num_members = num_members
-        #self.members = []
         self.build_tree()
+        self.sum = 1
+        self.assign_left_right(self.root)
 
     def build_tree(self):
         if self.num_members <= 0:
             return
         self.root = Member(id=1, parent=None)
-        #self.members.append(self.root)
         queue = [self.root]
         current_id = 2 
         while current_id <= self.num_members:
             current_member = queue.pop(0)
             if current_id <= self.num_members:
                 left_child = Member(id=current_id, parent=current_member)
+                left_child.position = 'Left'
                 current_member.left_member = left_child
                 queue.append(left_child)
-                #self.members.append(left_child)
                 current_id += 1
             if current_id <= self.num_members:
                 right_child = Member(id=current_id, parent=current_member)
+                right_child.position = 'Right'
                 current_member.right_member = right_child
                 queue.append(right_child)
-                #self.members.append(right_child)
                 current_id += 1
+
+    def assign_left_right(self, node):
+        if node is None:
+            return
+        node.left = self.sum
+        self.sum += 1
+        if node.left_member:
+            self.assign_left_right(node.left_member)
+        node.right = self.sum
+        self.sum += 1 
+        if node.right_member:
+            self.assign_left_right(node.right_member)
+        
 
     def display_tree(self):
         queue = [self.root]
         while queue:
             current_member = queue.pop(0)
             print(f'Member ID: {current_member.id}, ',
-                  f'Left: {current_member.left_member.id if current_member.left_member else None}, ',
-                  f'Right: {current_member.right_member.id if current_member.right_member else None}, ',
+                  f'Left member: {current_member.left_member.id if current_member.left_member else None}, ',
+                  f'Right member: {current_member.right_member.id if current_member.right_member else None}, ',
+                  f'left: {current_member.left if current_member.left else None}, ',
+                  f'right: {current_member.right if current_member.right else None}, ',
                   f'Parent: {current_member.parent.id if current_member.parent else None}')
             if current_member.left_member:
                 queue.append(current_member.left_member)
