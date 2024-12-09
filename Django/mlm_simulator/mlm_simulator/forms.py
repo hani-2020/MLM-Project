b7e6fc62d66571cc9e1bed352adbe7a7b1c93409
hani-2020 <haniroshan2002@gmail.com>
2024-12-09 22:08:31 +0530

    added front end for using amount

--- Django/mlm_simulator/mlm_simulator/forms.py
+++ Django/mlm_simulator/mlm_simulator/forms.py
@@ -1,9 +1,6 @@
 from django import forms
 
-#unit_field = ((1, 'currency'), ('2', 'percentage'))
-
 class BaseDetailCollectionForm(forms.Form):
-    # joining_package_fee = forms.FloatField()
     additional_product_price = forms.FloatField(required=False, initial=0)
     number_of_users = forms.IntegerField(required=True)
     sponsor_bonus = forms.FloatField(required=False, initial=0)
@@ -12,25 +9,16 @@ class BaseDetailCollectionForm(forms.Form):
     product_prices = forms.CharField(max_length=255, required=False)
     capping_amount = forms.FloatField(required=False, initial=0)
     capping_scope = forms.MultipleChoiceField(choices=((1, 'Binary bonus'), (2, 'Matching bonus'), (3, 'Sponsor bonus'), (4, 'Level bonus')),widget=forms.CheckboxSelectMultiple,required=False)
-    # sponsor_bonus_unit = forms.ChoiceField(choices=unit_field)
     business_expenses_per_member = forms.FloatField(required=False, initial=1)
     matching_bonus_per_level = forms.CharField(max_length=255, required=False)
     pool_bonus = forms.FloatField(required=False, initial=0)
     pool_distribution = forms.IntegerField(required=False, initial=1)
-    # business_expenses_unit = forms.ChoiceField(choices=unit_field)
-    # tax_per_person = forms.FloatField()
-    # tax_unit = forms.ChoiceField(choices=unit_field)
 
 class BinaryForm(BaseDetailCollectionForm):
     binary_bonus_range = forms.CharField(max_length=255, required=False)
     binary_bonus = forms.CharField(max_length=255, required=False)
-    # binary_bonus_unit = forms.ChoiceField(choices=unit_field)
     binary_bonus_pairing_ratio = forms.CharField(max_length=255, required=False)
-    # matching_bonus_per_level = forms.CharField(max_length=255, required=False)
-    # matching_bonus_unit = forms.ChoiceField(choices=unit_field)
-    # capping_scope = forms.MultipleChoiceField(choices=((1, 'Binary bonus'), (2, 'Matching bonus'), (3, 'Sponsor bonus')),widget=forms.CheckboxSelectMultiple,required=False)
 
 class UnilevelForm(BaseDetailCollectionForm):
-    # capping_scope = forms.MultipleChoiceField(choices=((4, 'Level bonus'), (2, 'Matching bonus')),widget=forms.CheckboxSelectMultiple,required=False)
     downlines_per_user = forms.IntegerField(required=False)
     level_bonus_per_level = forms.CharField(max_length=255, required=False)
